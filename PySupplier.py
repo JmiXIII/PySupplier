201c27d5f4890fe30d15018b3a3b2335649f7600
JmiXIII <sylvn.page@gmail.com>
2016-10-16 23:58:49 +0200

    STEP 2

--- PySupplier.py
+++ PySupplier.py
@@ -6,6 +6,7 @@ Created on Sun Oct  9 22:29:18 2016
 """
 import pandas as pd
 from datetime import datetime
+import xlwings as xl
 
 
 path = r'C:\Users\sylvn_000\Documents\GitHub\PySupplier'
@@ -21,6 +22,16 @@ start = datetime(2015, 12, 1)
 end = datetime(2016, 12, 31)
 ranges = pd.date_range(start, end, freq='M')
 rng = [x for x in ranges]
+list_sup = []
+
 sup_datas = suppliers_datas.groupby('RE_FOUR').get_group(175)
 sup_datas.LA_DECH = pd.to_datetime(sup_datas.LA_DECH)
-sup = sup_datas[(sup_datas.LA_DECH < rng[1]) & (sup_datas.LA_DECH > rng[0])]
+#writer = pd.ExcelWriter('test.xlsx', engine='xlsxwriter')
+sup_datas.to_excel(path+'/175.xls', '175')
+sup_ret = []
+writer = pd.ExcelWriter(path+'/'+sup_datas.FO_DESI+'.xls', engine='xlsxwriter')
+for i in range(len(rng)-1):
+    sup = sup_datas[(sup_datas.LA_DECH < rng[i + 1]) &
+                    (sup_datas.LA_DECH > rng[i])]
+    sup.to_excel(writer,str(i))
+    writer.save()
